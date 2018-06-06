68d0f70af2993f48f42eec2f56badae5d705864c
Smita Gupta <smita.gupta@equisoft.com>
2018-06-06 13:07:06 -0400

    module4

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,9 +12,7 @@ class Quiz extends Component {
   render() {
     const isQuizEnd = Boolean((this.state.quizposition - 1) != quizData.quiz_questions.length);
     return (
-        <div>
-          { isQuizEnd  ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> }
-        </div>
+        { isQuizEnd  ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> }
       )
   }
 }
