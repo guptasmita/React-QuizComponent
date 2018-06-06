0e4c17fdc8fa133089893bd0488efe71b006f93e
Smita Gupta <smita.gupta@equisoft.com>
2018-06-06 14:08:36 -0400

    module7

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,11 @@ class Quiz extends Component {
    super(props);
    this.state = { quiz_position: 1}
  }
+ handleResetClick() {
+   this.setState ({
+     quiz_position: 1
+   })
+ }
  showNextQuestion () {
    this.setState({
      quiz_position: this.state.quiz_position + 1
@@ -18,7 +23,7 @@ class Quiz extends Component {
     const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
     return (
         <div>
-          { isQuizEnd  ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler = {this.showNextQuestion.bind(this)} /> }
+          { isQuizEnd  ? <QuizEnd resetClickHandler = {this.handleResetClick.bind(this)}/> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler = {this.showNextQuestion.bind(this)} /> }
         </div>
       )
   }
